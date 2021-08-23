7594d5ac34ebc4aca309eca29107aa385bf3dad3
IRADUKUNDA Joseph <iradukundajose8@gmail.com>
2021-08-23 18:39:12 +0200

    adding javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+function validateEmail(mail){
+    var email=document.getElementById('email')
+    var result=document.getElementById('result')
+
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+&/.test(email.value)){
+        // alert('you entered' + email.value)
+        result.innerHTML='you entered'
++ value 
+return true; 
+   }
+   result.innerHTML = 'Please provide a valide email address'
+   return false;
+}
